165bb73ccb06fe214e9ec2436cf858c7ac4d9f9d
Peter Grimshaw <pagrimshaw@gmail.com>
2020-11-23 20:47:45 +0000

    Change json python script

--- debug_string.py
+++
@@ -1,9 +0,0 @@
-
-from decision_tree import tree_json, tidy_tree
-
-with open('data/decision_tree.txt') as f:
-    tree_text = f.read()
-
-tree_text = tidy_tree(tree_text, ['static_mean', 'sensitive_mean', 'btp_mean'])
-
-tree_json(tree_text)
---
+++ text_to_json.py
@@ -0,0 +1,10 @@
+
+from decision_tree import tree_json, tidy_tree
+import sys
+
+with open(sys.argv[1]) as f:
+    tree_text = f.read()
+
+tree_text = tidy_tree(tree_text, ['static_mean', 'sensitive_mean', 'btp_mean', 'num_neighbours'])
+
+tree_json(tree_text)
